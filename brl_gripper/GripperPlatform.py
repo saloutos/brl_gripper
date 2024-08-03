e2f1116e2cd34c2e4cff2456ebc9af7a569c4dc9
Sharmi <sharmis@mit.edu>
2024-08-03 16:40:47 -0400

    added E10

--- brl_gripper/GripperPlatform.py
+++ brl_gripper/GripperPlatform.py
@@ -74,10 +74,14 @@ class GripperPlatform:
 
             if sensor_mode: #could add names in a separate config file instead of hardcoding here
                 rnn_model_fname_lsensor = "2024-07-09_18-04-38_E9_6_38_and_E9_7_3_BinnedFulloutRNN_hd48_H512_k64_bpi32_lr0p0005"
+                # rnn_model_fname_lsensor = "2024-07-31_15-11-33_E10_07_30_RNN"
+
                 self.nn_model_lsensor, self.std_dev_X_lsensor, self.mean_X_lsensor = load_model(rnn_model_fname_lsensor)
                 self.h_lsensor, self.theta_angles_lsensor, self.phi_angles_lsensor = init_run_binned_rnn(self.nn_model_lsensor)
 
-                rnn_model_fname_rsensor = "2024-07-25_14-29-20_E8_7_8_RNN"
+                rnn_model_fname_rsensor = "2024-07-31_15-11-33_E10_07_30_RNN"
+                # rnn_model_fname_rsensor = "2024-07-09_18-04-38_E9_6_38_and_E9_7_3_BinnedFulloutRNN_hd48_H512_k64_bpi32_lr0p0005"
+
                 self.nn_model_rsensor, self.std_dev_X_rsensor, self.mean_X_rsensor = load_model(rnn_model_fname_rsensor) 
                 self.h_rsensor, self.theta_angles_rsensor, self.phi_angles_rsensor = init_run_binned_rnn(self.nn_model_rsensor)
 
@@ -824,7 +828,7 @@ class GripperPlatform:
             #evaluate model and return values for sensor 2
             sensor_data_right, h_right = run_binned_rnn(pressure_vals,self.nn_model_rsensor, self.std_dev_X_rsensor, self.mean_X_rsensor, self.theta_angles_rsensor, self.phi_angles_rsensor, self.h_rsensor)
             self.h_rsensor = h_right
-            # print("[" + " ".join(f"{value:.8f}" for value in sensor_data_right) + "]")
+            print("[" + " ".join(f"{value:.8f}" for value in sensor_data_right) + "]")
             return sensor_data_right[0:-1]
             # return np.array([0,0,0,10,10])
 
