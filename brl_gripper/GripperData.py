aa6061f729246614e4cc0c1534d3321e73ac741d
Sharmi <sharmis@mit.edu>
2024-08-09 10:52:15 -0400

    added ellipsoid assets

--- brl_gripper/GripperData.py
+++ brl_gripper/GripperData.py
@@ -205,6 +205,163 @@ class FingertipSensorData(SensorData):
         scene.geoms[idx].rgba=np.array([1, 0, 1, 0.5])
         idx += 1
         return idx
+    
+class EllipsoidFingertipSensorData(SensorData):
+    def __init__(self, name=""):
+        super().__init__(name)
+        # NOTE: all sensor data should be stored as 1-D numpy arrays
+        self.contact_force_offset = np.array([0.0, 0.0, 0.0])
+        self.contact_force_raw = np.array([0.0, 0.0, 0.0])
+        self.contact_angle_raw = np.array([0.0, 0.0])
+        self.contact_force = np.array([0.0, 0.0, 0.0]) # fx, fy, fz in N  # fz is normal to surface
+        self.contact_angle = np.array([0.0, 0.0])  # theta, phi in deg
+        # TODO: set a clearer ToF order here
+        self.tof_raw = np.array([0, 0, 0, 0, 0])  # 1,2,3,4,5 in mm
+        self.dist_offset = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+        self.dist = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) # 1,2,3,4,5 in m
+
+        # TODO: save some intermediate variables here, other useful quantities?
+        # contact location in sensor frame
+        # contact frame as rotation matrix
+        # contact force in sensor frame
+        self.T_sensor_contact = np.eye(4) # transformation from sensor frame to contact frame
+        self.F_contact_sensor = np.array([0.0, 0.0, 0.0]) # contact force in sensor frame
+
+        # contact angle ranges and filter coefficients
+        self.ellipse_params = np.array([10.5, 9, 6.35])
+        self.theta_range = [-34.0, 34.0]
+        self.phi_range = [-55.0, 52.0]
+        self.contact_force_filter_alpha = 1.0
+        self.contact_angle_filter_alpha = 1.0
+        self.normal_force_threshold = 1.0
+        # variables for visualization
+        # NOTE: center of rubber sphere is origin of sensor frame
+        self.nominal_contact = np.array([0.0, 0.0, 0.01]) # sphere radius is 10mm
+        self.force_scale = 0.02
+        self.tof_pos_offsets = np.array([[-0.0025, -0.00782, -0.008], # tof1
+                                    [0.0082, -0.00735, 0.0], # tof2
+                                    [-0.0025, 0.00782, -0.008], # tof3
+                                    [0.0082, 0.00735, 0.0], # tof4
+                                    [-0.0015, 0.0, -0.011]]) # tof5
+        self.tof_axes = [0, 2, 0, 2, 2]
+        self.tof_signs = [-1, 1, -1, 1, -1]
+
+    # logging functions
+    def log_data(self):
+        return self.contact_force.tolist()+self.contact_angle.tolist()+self.dist.tolist()
+    def log_header(self):
+        return [self.name+"_fx",self.name+"_fy",self.name+"_fz",
+                self.name+"_theta",self.name+"_phi",
+                self.name+"_dist1",self.name+"_dist2",self.name+"_dist3",self.name+"_dist4",self.name+"_dist5"]
+    # initialization functions
+    def set_force_offset(self, offset=None):
+        if offset is not None:
+            self.contact_force_offset = offset
+        else:
+            self.contact_force_offset = self.contact_force_raw.copy()
+    def set_dist_offset(self, offset=None):
+        if offset is not None:
+            self.dist_offset = offset
+        else:
+            self.dist_offset = self.dist.copy()
+    # processing sensor data
+    def update_raw_data_from_hw(self, new_data):
+        # new data should come in as list of arrays
+        self.contact_force_raw = new_data[0]
+        self.contact_angle_raw = new_data[1]
+        self.tof_raw = new_data[2]
+    def update_raw_data_from_sim(self, new_data):
+        # new data should come in as dict with mujoco sensor names and data as arrays
+        sim_force_data = new_data['force']
+        sim_contact_vec = new_data['contact']
+        sim_tof_data = np.array([new_data['tof1'],new_data['tof2'],new_data['tof3'],new_data['tof4'],new_data['tof5']]).squeeze()
+        # process contact and force data to re-create raw hardware data
+        # contact location should use angles from vector spanning origin to contact location
+        theta_rad = np.arcsin(-sim_contact_vec[1])
+        phi_rad = np.arctan2(sim_contact_vec[0], sim_contact_vec[2])
+        theta_deg  = np.clip(np.rad2deg(theta_rad), self.theta_range[0], self.theta_range[1])
+        phi_deg = np.clip(np.rad2deg(phi_rad), self.phi_range[0], self.phi_range[1])
+        self.contact_angle_raw = np.array([theta_deg, phi_deg])
+        # force vector should be in frame that is normal to the surface of the ellipsoid
+        contact_vec_normal = 2*np.array(sim_contact_vec[0]/self.ellipse_params[0]**2,sim_contact_vec[1]/self.ellipse_params[1]**2,sim_contact_vec[2]/self.ellipse_params[2]**2) #find vector normal to surface at contact location
+        theta_rad_normal = np.arcsin(-contact_vec_normal[1])
+        phi_rad_normal = np.arctan2(contact_vec_normal[0], contact_vec_normal[2])
+        R_theta = np.array([[1, 0, 0], [0, np.cos(theta_rad_normal), -np.sin(theta_rad_normal)], [0, np.sin(theta_rad_normal), np.cos(theta_rad_normal)]]) # Rx by theta
+        R_phi = np.array([[np.cos(phi_rad_normal), 0, np.sin(phi_rad_normal)], [0, 1, 0], [-np.sin(phi_rad_normal), 0, np.cos(phi_rad_normal)]]) # Ry by phi
+        R_cont = R_phi @ R_theta
+        self.contact_force_raw = -1.0 * (R_cont.T @ sim_force_data.reshape((3,1))).squeeze()
+        # process tof data to re-create raw hardware data
+        sim_tof_data = np.where(sim_tof_data==-1.0, 0.255, sim_tof_data)
+        self.tof_raw = np.round(1000.0*sim_tof_data)
+    def process_data(self):
+        self.filter_contact_force()
+        # TODO: only filter angle if contact force is above threshold
+        self.filter_contact_angle()
+        self.convert_tof_data()
+        # update other contact variables # TODO: verify this is correct
+        theta_rad = np.deg2rad(self.contact_angle[0])
+        phi_rad = np.deg2rad(self.contact_angle[1])
+        R_theta = np.array([[1, 0, 0], [0, np.cos(theta_rad), -np.sin(theta_rad)], [0, np.sin(theta_rad), np.cos(theta_rad)]]) # Rx by theta
+        R_phi = np.array([[np.cos(phi_rad), 0, np.sin(phi_rad)], [0, 1, 0], [-np.sin(phi_rad), 0, np.cos(phi_rad)]]) # Ry by phi
+        R_cont = R_phi @ R_theta
+        self.contact_force_sensor = R_cont @ self.contact_force
+        self.T_sensor_contact[0:3,0:3] = R_cont
+        self.T_sensor_contact[0:3,3:4] = R_cont.dot(self.nominal_contact).reshape((3,1))
+
+    def filter_contact_force(self, alpha=None):
+        if alpha is not None:
+            self.contact_force = (1.0-alpha)*self.contact_force + alpha*(self.contact_force_raw-self.contact_force_offset)
+        else:
+            self.contact_force = (1.0-self.contact_force_filter_alpha)*self.contact_force + self.contact_force_filter_alpha*(self.contact_force_raw-self.contact_force_offset)
+    def filter_contact_angle(self, alpha=None):
+        if alpha is not None:
+            self.contact_angle = (1.0-alpha)*self.contact_angle + alpha*self.contact_angle_raw
+        else:
+            self.contact_angle = (1.0-self.contact_angle_filter_alpha)*self.contact_angle + self.contact_angle_filter_alpha*self.contact_angle_raw
+    def convert_tof_data(self):
+        self.tof_raw = np.where(self.tof_raw==0, 254, self.tof_raw)
+        self.dist = (self.tof_raw/1000.0) - self.dist_offset  # convert mm to m, apply offset
+    # visualization of hardware data
+    def sync_data_to_viewer(self, scene, start_idx):
+        # TODO: check self.kinematics, where is it set and is rotation matrix correct?
+
+        idx = start_idx
+        # visualize data for 5 ToF sensors
+        for t in range(5):
+            # line starts at pos, aligns with z-axis of sensor frame
+            scene.geoms[idx].type = 103 # line
+            scene.geoms[idx].size = np.array([4, 4, self.dist[t]]) # width in px, width in px, length in m
+            scene.geoms[idx].pos = self.kinematics[0] + self.kinematics[1].dot(self.tof_pos_offsets[t,:])
+            # align z-axis geom frame with tof sensor direction!
+            z = self.tof_signs[t]*self.kinematics[1][:,self.tof_axes[t]].reshape((3,1)) # should be unit vec already
+            y = np.cross(z.squeeze(), np.array([0, 1, 0]).squeeze()).reshape((3,1))
+            x = np.cross(y.squeeze(), z.squeeze()).reshape((3,1))
+            scene.geoms[idx].mat = np.hstack((x/np.linalg.norm(x), y/np.linalg.norm(y), z/np.linalg.norm(z))) # full 3x3 matrix, not 9-vector
+            scene.geoms[idx].rgba = np.array([1, 0.2, 0, 0.5])
+            idx += 1
+        # visualize force at contact location
+        # arrow starts at pos, aligns with z-axis of sensor frame
+        scene.geoms[idx].type = 100 # arrow
+        scene.geoms[idx].size = np.array([0.002, 0.002, self.force_scale*np.linalg.norm(self.contact_force)]) # radius, radius, length in m
+        # build contact frame from two angles
+        theta = np.deg2rad(self.contact_angle[0])
+        phi = np.deg2rad(self.contact_angle[1])
+        R_theta = np.array([[1, 0, 0], [0, np.cos(theta), -np.sin(theta)], [0, np.sin(theta), np.cos(theta)]]) # Rx by theta
+        R_phi = np.array([[np.cos(phi), 0, np.sin(phi)], [0, 1, 0], [-np.sin(phi), 0, np.cos(phi)]]) # Ry by phi
+        R_cont = R_phi @ R_theta
+        p_cont = R_cont.dot(self.nominal_contact)
+        scene.geoms[idx].pos = self.kinematics[0] + self.kinematics[1].dot(p_cont)
+        # align z-axis geom frame with contact force direction!
+        if np.linalg.norm(self.contact_force)>0.01:
+            z = self.kinematics[1] @ R_cont @ (self.contact_force.reshape((3,1))/np.linalg.norm(self.contact_force))
+            y = np.cross(z.squeeze(), np.array([0, 0, 1]).squeeze()).reshape((3,1))
+            x = np.cross(y.squeeze(), z.squeeze()).reshape((3,1))
+            scene.geoms[idx].mat = np.hstack((x/np.linalg.norm(x), y/np.linalg.norm(y), z/np.linalg.norm(z))) # full 3x3 matrix, not 9-vector
+        else:
+            scene.geoms[idx].mat = self.kinematics[1] @ R_cont # full 3x3 matrix, not 9-vector
+        scene.geoms[idx].rgba=np.array([1, 0, 1, 0.5])
+        idx += 1
+        return idx
 
 class PhalangeSensorData(SensorData):
     def __init__(self, name=""):
